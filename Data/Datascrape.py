57921f1ea286e85905718f42e4e536683f7b1acb
Ethan Batiuk <ebatiuk@uwaterloo.ca>
2025-06-08 17:18:05 -0400

    hid key and regenrated a new one

--- Data/Datascrape.py
+++ Data/Datascrape.py
@@ -1,11 +1,10 @@
 """
 File for scraping data using Polygon.io
-Key: UZUm8ezpPOTHnfneTdyJlasqlN6ptACd
 """
 
 from polygon import RESTClient as Rest
 
-api_key = 'UZUm8ezpPOTHnfneTdyJlasqlN6ptACd'
+
 client = Rest(api_key)
 
 response = client.get_previous_close_agg("AMD")
