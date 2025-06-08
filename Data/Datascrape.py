a198ab1d1746dc6bf7d22fb956c4a005ef90b370
Ethan Batiuk <ebatiuk@uwaterloo.ca>
2025-06-08 17:11:23 -0400

    fixed grammar?

--- Data/Datascrape.py
+++ Data/Datascrape.py
@@ -5,10 +5,10 @@ Key: UZUm8ezpPOTHnfneTdyJlasqlN6ptACd
 
 from polygon import RESTClient as Rest
 
-api_key = UZUm8ezpPOTHnfneTdyJlasqlN6ptACd
-client = Rest(UZUm8ezpPOTHnfneTdyJlasqlN6ptACd)
+api_key = 'UZUm8ezpPOTHnfneTdyJlasqlN6ptACd'
+client = Rest(api_key)
 
-response = client.get_previous_close("AMD")
+response = client.get_previous_close_agg("AMD")
 
 print("Previous Close Data for AMD:")
 print(response)
