2983ae82b5d04d8cffe8e052f26de5d95f991924
Srividhu Potluri <124634566+spotlur2@users.noreply.github.com>
2025-06-09 15:07:42 -0400

    updated datascrape to function with key in env

--- Data/Datascrape.py
+++ Data/Datascrape.py
@@ -3,9 +3,15 @@ File for scraping data using Polygon.io
 """
 
 from polygon import RESTClient as Rest
+from dotenv import load_dotenv
+import os
 
+load_dotenv(dotenv_path = ".env.local")
+API_KEY = os.getenv("POLYGON_API_KEY")
+if not API_KEY:
+    raise ValueError("put the api key in the .env.local file")
 
-client = Rest(api_key)
+client = Rest(API_KEY)
 
 response = client.get_previous_close_agg("AMD")
 
